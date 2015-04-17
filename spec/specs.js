3ed40f6bc7d3dee8088595b3cfad6fb367af998d
chitra <chitra.atmakuru@gmail.com>
2015-04-17 12:23:51 -0700

    Add index.html

--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,7 @@ describe("PizzaType", function(){
         it("calculates cost for 2 quantity", function(){
         var pizza1 = Object.create(PizzaType);
         pizza1.init();
-        pizza1.type = "pepporoni";
+        
         pizza1.quantity = 2;
         expect(pizza1.cost()).to.equal(40);
     });
@@ -24,6 +24,15 @@ describe("PizzaType", function(){
         pizza1.type = "cheese ";
         pizza1.quantity = 1;
         expect(pizza1.cost()).to.equal(15);
+    });
+    });
+
+    describe("cost()",function(){
+        it("calculates cost for 1 quantity", function(){
+        var pizza1 = Object.create(PizzaType);
+        pizza1.type = "cheese ";
+        pizza1.quantity = 3;
+        expect(pizza1.cost()).to.equal(45);
     });
     });
 
